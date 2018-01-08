3017f47c6fb38fbc60cc5e17f1bd49cb9dda8c0d
zhihuikaicheng <314696702@qq.com>
2018-01-08 15:54:18 +0800

    zkc

---
+++ nets/my_model.py
@@ -0,0 +1,178 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import pdb
+
+from nets import inception_v3
+
+FLAGS = tf.app.flags.FLAGS
+slim = tf.contrib.slim
+
+class BaseModel(object):
+    def __init__(self):
+        '''
+        we should declear:
+        self.logits
+        self.end_points
+        self.loss
+        self.acc
+        '''
+
+class MyInception(BaseModel):
+    def __init__(self, num_classes, sizes, scope, is_training=True):
+        self.num_classes = num_classes
+        self.sizes = sizes
+        self.scope = scope
+        self.is_training = is_training
+
+        with tf.variable_scope(scope):
+            self.init_input()
+            with slim.arg_scope(inception_v3.inception_v3_arg_scope()):
+                self.init_network()
+            self.init_loss()
+
+    def init_input(self):
+        self.image = tf.placeholder(tf.float32, [None, FLAGS.origin_height, FLAGS.origin_width, FLAGS.origin_channel])
+        self.label = tf.placeholder(tf.float32, [None, self.num_classes])
+
+    def init_network(self):
+        self.sub_models = []
+        # feature = []
+        for i,s in enumerate(self.sizes):
+            sub_model = SubIncption([self.image,self.label],
+                self.num_classes, s, 'branch_{}'.format(i), is_training=self.is_training)
+            self.sub_models.append(sub_model)
+            # feature.append(sub_model.end_points['Mixed_7c'])
+        # pdb.set_trace()
+        # self.feature = tf.concat(feature, axis=-1)
+
+        if len(self.sizes) > 1:
+            joint_scope = 'joint'
+
+            with tf.variable_scope(joint_scope):
+                x = tf.concat([model.logits for model in self.sub_models], axis=-1)
+                x = slim.fully_connected(x, self.num_classes, normalizer_fn=None, scope='joint_fc')
+                self.joint_logits = x
+                self.joint_pred = tf.nn.softmax(x)
+
+                corr_pred = tf.equal(tf.argmax(self.label,1), tf.argmax(self.joint_logits,1))
+                self.joint_acc = tf.reduce_sum(tf.cast(corr_pred, tf.int32))
+
+                self.logits = self.joint_logits
+                self.end_points = {}
+                for model in self.sub_models:
+                    for end_point in model.end_points:
+                        self.end_points[model.scope+'/'+end_point] = model.end_points[end_point]
+                self.end_points[joint_scope+'/Logits'] = self.joint_logits
+                self.end_points[joint_scope+'/Predictions'] = self.joint_pred
+
+
+            tf.summary.histogram('activations/%s/%s'%(self.scope,'Logits'), self.joint_logits)
+            tf.summary.scalar('sparsity/%s/%s'%(self.scope,'Logits'), tf.nn.zero_fraction(self.joint_logits))
+            tf.summary.histogram('activations/%s/%s'%(self.scope,'Predictions'), self.joint_pred)
+            tf.summary.scalar('sparsity/%s/%s'%(self.scope,'Predictions'), tf.nn.zero_fraction(self.joint_pred))
+
+            tf.summary.scalar('acc/%s' % self.scope, self.joint_acc)
+        else:
+            self.logits = self.sub_models[0].logits
+            self.end_points = self.sub_models[0].end_points
+
+    def init_loss(self):
+        cross_entropy = tf.reduce_sum([model.loss for model in self.sub_models])
+
+        if len(self.sizes) > 1:
+            joint_cross_entropy = -tf.reduce_sum(self.label*tf.log(self.joint_pred+FLAGS.opt_epsilon), axis=1)
+            joint_cross_entropy = tf.reduce_mean(joint_cross_entropy)
+            cross_entropy = cross_entropy + joint_cross_entropy
+
+            tf.summary.scalar('losses/%s_joint' % self.scope, joint_cross_entropy)
+
+        regular_vars = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        regularizers = tf.add_n(regular_vars)
+
+        self.loss = cross_entropy + FLAGS.weight_decay * regularizers
+        cross_entropy_branch_0 = -tf.reduce_sum(self.joint_pred*tf.log(self.sub_models[0].pred+FLAGS.opt_epsilon), axis=1)
+        self.loss_branch_0 = tf.reduce_mean(cross_entropy)
+        cross_entropy_branch_1 = -tf.reduce_sum(self.joint_pred*tf.log(self.sub_models[1].pred+FLAGS.opt_epsilon), axis=1)
+        self.loss_branch_1 = tf.reduce_mean(cross_entropy)
+
+        tf.summary.scalar('losses/%s_cross_entropy' % self.scope, cross_entropy)
+        tf.summary.scalar('losses/%s_regularizers' % self.scope, regularizers)
+        tf.summary.scalar('losses/%s' % self.scope, self.loss)
+
+    def load_pretrain_model(self, sess, path):
+        # make sure self.scope is the root scope
+        for model in self.sub_models:
+            model.load_pretrain_model(sess, path, self.scope)
+
+class SubIncption(BaseModel):
+    def __init__(self, input, num_classes, size, scope, is_training=True):
+        self.image = input[0]
+        self.label = input[1]
+        self.num_classes = num_classes
+        self.size = size
+        self.scope = scope
+        self.is_training = is_training
+
+        with tf.variable_scope(self.scope):
+            self.init_network()
+            self.init_loss()
+
+    def init_network(self):
+        x = self.image
+        x = tf.image.resize_images(x, [self.size,self.size], 0) #0 mean bilinear
+        x = tf.subtract(x, 0.5)
+        x = tf.multiply(x, 2.0)
+
+        logits,end_points = inception_v3.inception_v3(x,
+            num_classes=self.num_classes,
+            is_training=self.is_training,
+        )
+
+        self.logits = logits
+        self.pred = end_points['Predictions']
+        self.end_points = end_points
+
+        corr_pred = tf.equal(tf.argmax(self.label,1), tf.argmax(self.logits,1))
+        self.acc = tf.reduce_sum(tf.cast(corr_pred, tf.int32))
+
+        for end_point in self.end_points:
+            x = self.end_points[end_point]
+            tf.summary.histogram('activations/%s/%s'%(self.scope,end_point), x)
+            tf.summary.scalar('sparsity/%s/%s'%(self.scope,end_point), tf.nn.zero_fraction(x))
+
+        tf.summary.scalar('acc/%s' % self.acc, self.acc)
+
+    def init_loss(self):
+        cross_entropy = -tf.reduce_sum(self.label*tf.log(self.pred+FLAGS.opt_epsilon), axis=1)
+        self.loss = tf.reduce_mean(cross_entropy)
+
+        tf.summary.scalar('losses/%s' % self.scope, self.loss)
+
+    def load_pretrain_model(self, sess, path, father_scope):
+        '''
+        in pretrain, name like:InceptionV3/Mixed_7b/Branch_2/Conv2d_0d_3x1/weights
+        in our model, name like:inception_v3/branch_0/InceptionV3/Mixed_7b/Branch_2/Conv2d_0d_3x1/weights:0
+        so, model_name = inception_v3/branch_0/ + pretrain_name + ':0'
+                       = father_scope/sub_model_scope/pretrain_name:0
+
+        note:
+        some vars can't be load like final-fc
+        in pretrain, name of these vars like:InceptionV3/AuxLogits/Conv2d_2b_1x1/biases
+                                             InceptionV3/Logits/Conv2d_2b_1x1/biases
+        they all have prefix like InceptionV3/AuxLogits/ or InceptionV3/Logits/
+        '''
+        scope = father_scope + '/' + self.scope + '/'
+        variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope)
+
+        d = {}
+        for var in variables:
+            name = var.name.replace(scope, '').replace(':0', '')
+            if name.startswith('InceptionV3/AuxLogits') or name.startswith('InceptionV3/Logits'):
+                continue
+            d[name] = var
+
+        saver = tf.train.Saver(d)
+        saver.restore(sess, path)
--- scripts/finetune-inception_v3_on_market-1501_299.sh
+++ scripts/finetune-inception_v3_on_market-1501_299.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 # Where the pre-trained InceptionV3 checkpoint is saved to.
-PRETRAINED_CHECKPOINT_DIR=/home/yuanziyi/checkpoints
+PRETRAINED_CHECKPOINT_DIR=/world/data-gpu-94/sysu-reid/zhangkaicheng/checkpoints
 # Where the training (fine-tuned) checkpoint and logs will be saved to.
 TRAIN_DIR=/world/data-gpu-94/sysu-reid/zhangkaicheng/checkpoints/inception_v3_299
 # Where the dataset is saved to.
-DATASET_DIR=/home/yuanziyi/Market-1501
+DATASET_DIR=/world/data-gpu-94/sysu-reid/zhangkaicheng/Market-1501
 # WHere the log is saved to
-LOG_DIR=/home/yuanziyi/log
+LOG_DIR=/world/data-gpu-94/sysu-reid/zhangkaicheng/log
 # Wher the tfrecord file is save to
-OUTPUT_DIR=/home/yuanziyi/Market-1501-tfrecord/bounding_box_test
-python ../get_feature/get_gallery_features_single.py \
---learning_rate=2e-3 \
+OUTPUT_DIR=/world/data-gpu-94/sysu-reid/zhangkaicheng/Market-1501-tfrecord/bounding_box_train
+python train_image_classifier_299.py \
+--learning_rate=2e-4 \
 --learning_rate_decay_type=fixed \
 --dataset_name=Market_1501 \
 --dataset_split_name=train \
 --dataset_dir=${OUTPUT_DIR} \
 --model_name=inception_v3 \
 --batch_size=8 \
---max_number_of_steps=10001 \
+--max_number_of_steps=20000 \
 --checkpoint_dir=${TRAIN_DIR} \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt \
 --log_dir=${LOG_DIR} \
---save_model_summary_secs=6 \
---log_every_n_steps=5 \
+--save_model_summary_secs=300 \
+--log_every_n_steps=100 \
 --optimizer=adam \
 --adam_beta1=0.5 \
 --adam_beta2=0.999 \
