9bca5c01258b8f739551be94ff645a6494a95de5
zhihuikaicheng <314696702@qq.com>
2018-01-09 14:40:31 +0800

    add eval

---
+++ scripts/eval.sh
@@ -0,0 +1,8 @@
+
+python eval.py \
+--label_gallery_path=/home/zhangkaicheng/data/test_gallery_labels.mat \
+--feature_gallery_path=/home/zhangkaicheng/data/test_gallery_features.mat \
+--label_probe_path=/home/zhangkaicheng/data/test_probe_labels.mat \
+--feature_probe_path=/home/zhangkaicheng/data/test_probe_features.mat \
+--cam_gallery_path=/home/zhangkaicheng/data/testCAM.mat \
+--cam_probe_path=/home/zhangkaicheng/data/queryCAM.mat 
