d117ce9c73118df20526c6723781bf4ae2ac74fc
zhihuikaicheng <314696702@qq.com>
2018-01-10 15:22:55 +0800

    zkc change convert_to_tfrecord

--- convert_to_tfrecord.py
+++ convert_to_tfrecord.py
@@ -27,13 +27,22 @@ def _int64_feature(value):
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-def worker(args, dict_labels):
-    img_names, output_file = args
-    
+def main():
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    file_names = os.listdir(input_path)
+    file_names = [name for name in file_names if name.endswith('jpg')]
+    random.shuffle(file_names)
+
+    output_file = os.path.join(output_path, 'm_{}.tfrecords'.format(0))
     writer = tf.python_io.TFRecordWriter(output_file)
 
-    imgs = [skimage.io.imread(os.path.join(input_path,name)) for name in img_names]
+    imgs = [skimage.io.imread(os.path.join(input_path,name)) for name in file_names]
     labels = [int(name[:name.find('_')]) for name in file_names]
+    unique_labels = list(set(labels))
+    unique_labels.sort()
+    dict_labels = dict(zip(unique_labels, range(len(unique_labels))))
 
     for i in range(len(labels)):
         img = imgs[i]
@@ -49,41 +58,6 @@ def worker(args, dict_labels):
         writer.write(example.SerializeToString())
 
     writer.close()
-
-    return 0
-
-def main():
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    file_names = os.listdir(input_path)
-    file_names = [name for name in file_names if name.endswith('jpg')]
-    random.shuffle(file_names)
-
-    labels = [int(name[:name.find('_')]) for name in file_names]
-    unique_labels = list(set(labels))
-    unique_labels.sort()
-    dict_labels = dict(zip(unique_labels, range(len(unique_labels))))
-
-    n = len(file_names)//num_file
-
-    pool_args = []
-    for i in range(num_file):
-        p_args = []
-        ibeg = i * n
-        iend = ibeg + n
-        p_args.append(file_names[ibeg:iend])
-        p_args.append(os.path.join(output_path, 'm_{}.tfrecords'.format(i)))
-        pool_args.append(p_args)
-
-    pool = multiprocessing.Pool(num_workers)
-
-    #pdb.set_trace()
-
-    pool.map(worker, pool_args, dict_labels)
-    pool.close()
-    pool.join()
-    #map(worker, pool_args)
 
 if __name__ == "__main__":
     main()
