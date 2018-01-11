43f2bea5a2d176d9bed773fbabf96032a156770e
Frankziyi <617328440@qq.com>
2018-01-11 16:31:00 +0800

    blahblahblah

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,8 +1,24 @@
 
 python eval.py \
---label_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/test_gallery_labels.mat \
---feature_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/test_gallery_features.mat \
---label_probe_path=/world/data-gpu-94/sysu-reid/multiscale/test_probe_labels.mat \
---feature_probe_path=/world/data-gpu-94/sysu-reid/multiscale/test_probe_features.mat \
---cam_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/testCAM.mat \
---cam_probe_path=/world/data-gpu-94/sysu-reid/multiscale/queryCAM.mat 
+--label_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/13869/test_gallery_labels.mat \
+--feature_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/13869/test_gallery_features.mat \
+--label_probe_path=/world/data-gpu-94/sysu-reid/multiscale/13869/test_probe_labels.mat \
+--feature_probe_path=/world/data-gpu-94/sysu-reid/multiscale/13869/test_probe_features.mat \
+--cam_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/13869/testCAM.mat \
+--cam_probe_path=/world/data-gpu-94/sysu-reid/multiscale/13869/queryCAM.mat 
+
+python eval.py \
+--label_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/25602/test_gallery_labels.mat \
+--feature_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/25602/test_gallery_features.mat \
+--label_probe_path=/world/data-gpu-94/sysu-reid/multiscale/25602/test_probe_labels.mat \
+--feature_probe_path=/world/data-gpu-94/sysu-reid/multiscale/25602/test_probe_features.mat \
+--cam_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/25602/testCAM.mat \
+--cam_probe_path=/world/data-gpu-94/sysu-reid/multiscale/25602/queryCAM.mat 
+
+python eval.py \
+--label_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/36742/test_gallery_labels.mat \
+--feature_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/36742/test_gallery_features.mat \
+--label_probe_path=/world/data-gpu-94/sysu-reid/multiscale/36742/test_probe_labels.mat \
+--feature_probe_path=/world/data-gpu-94/sysu-reid/multiscale/36742/test_probe_features.mat \
+--cam_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/36742/testCAM.mat \
+--cam_probe_path=/world/data-gpu-94/sysu-reid/multiscale/36742/queryCAM.mat 
