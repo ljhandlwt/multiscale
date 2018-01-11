114f07c6514b9d6c1fb7f17ffac5b8e2ed0fb03c
Frankziyi <617328440@qq.com>
2018-01-11 15:38:21 +0800

    blahblahblah

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,8 +1,8 @@
 
 python eval.py \
---label_gallery_path=/world/data-gpu-94/sysu-reid/zhangkaicheng/data/test_gallery_labels.mat \
---feature_gallery_path=/world/data-gpu-94/sysu-reid/zhangkaicheng/data/test_gallery_features.mat \
---label_probe_path=/world/data-gpu-94/sysu-reid/zhangkaicheng/data/test_probe_labels.mat \
---feature_probe_path=/world/data-gpu-94/sysu-reid/zhangkaicheng/data/test_probe_features.mat \
---cam_gallery_path=/world/data-gpu-94/sysu-reid/zhangkaicheng/data/testCAM.mat \
---cam_probe_path=/world/data-gpu-94/sysu-reid/zhangkaicheng/data/queryCAM.mat 
+--label_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/test_gallery_labels.mat \
+--feature_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/test_gallery_features.mat \
+--label_probe_path=/world/data-gpu-94/sysu-reid/multiscale/test_probe_labels.mat \
+--feature_probe_path=/world/data-gpu-94/sysu-reid/multiscale/test_probe_features.mat \
+--cam_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/testCAM.mat \
+--cam_probe_path=/world/data-gpu-94/sysu-reid/multiscale/queryCAM.mat 
