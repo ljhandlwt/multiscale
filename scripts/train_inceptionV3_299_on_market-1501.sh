ab1047b894278e1354fc615146a056f6670c35dc
智慧 <zhihuikaicheng@zhihuideMacBook-Pro.local>
2018-01-11 12:53:05 +0800

    zkc change sh

--- scripts/train_inceptionV3_299_on_market-1501.sh
+++ scripts/train_inceptionV3_299_on_market-1501.sh
@@ -10,58 +10,52 @@ LOG_DIR=/world/data-gpu-94/sysu-reid/zhangkaicheng/log
 # Wher the tfrecord file is save to
 OUTPUT_DIR=/world/data-gpu-94/sysu-reid/zhangkaicheng/Market-1501-tfrecord/bounding_box_train
 python train_inceptionV3_299.py \
---learning_rate=2e-4 \
+--learning_rate=1e-2 \
 --learning_rate_decay_type=fixed \
 --dataset_name=Market_1501 \
 --dataset_split_name=train \
 --dataset_dir=${OUTPUT_DIR} \
 --model_name=inception_v3 \
 --batch_size=8 \
---max_number_of_steps=15000 \
+--max_number_of_steps=16000 \
 --checkpoint_dir=${TRAIN_DIR} \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt \
 --log_dir=${LOG_DIR} \
 --save_model_summary_secs=300 \
 --log_every_n_steps=100 \
---optimizer=adam \
---adam_beta1=0.5 \
---adam_beta2=0.999 \
+--optimizer=sgd \
 --weight_decay=0.00004
 
 python train_inceptionV3_299.py \
---learning_rate=5e-5 \
+--learning_rate=1e-3 \
 --learning_rate_decay_type=fixed \
 --dataset_name=Market_1501 \
 --dataset_split_name=train \
 --dataset_dir=${OUTPUT_DIR} \
 --model_name=inception_v3 \
 --batch_size=8 \
---max_number_of_steps=25000 \
+--max_number_of_steps=30000 \
 --checkpoint_dir=${TRAIN_DIR} \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt \
 --log_dir=${LOG_DIR} \
 --save_model_summary_secs=300 \
 --log_every_n_steps=100 \
---optimizer=adam \
---adam_beta1=0.5 \
---adam_beta2=0.999 \
+--optimizer=sgd \
 --weight_decay=0.00004
 
 python train_inceptionV3_299.py \
---learning_rate=1e-5 \
+--learning_rate=1e-4 \
 --learning_rate_decay_type=fixed \
 --dataset_name=Market_1501 \
 --dataset_split_name=train \
 --dataset_dir=${OUTPUT_DIR} \
 --model_name=inception_v3 \
 --batch_size=8 \
---max_number_of_steps=35000 \
+--max_number_of_steps=40000 \
 --checkpoint_dir=${TRAIN_DIR} \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt \
 --log_dir=${LOG_DIR} \
 --save_model_summary_secs=300 \
 --log_every_n_steps=100 \
---optimizer=adam \
---adam_beta1=0.5 \
---adam_beta2=0.999 \
+--optimizer=sgd \
 --weight_decay=0.00004
