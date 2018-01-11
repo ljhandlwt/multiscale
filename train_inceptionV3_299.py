73d1fa2dadfaae1dd2c433f196e5e07d49e72c48
智慧 <zhihuikaicheng@zhihuideMacBook-Pro.local>
2018-01-11 11:18:51 +0800

    zkc

--- train_inceptionV3_299.py
+++ train_inceptionV3_299.py
@@ -321,7 +321,7 @@ class Trainer(object):
         img_height = tf.cast(features['img_height'], tf.int32)
         img_width = tf.cast(features['img_width'], tf.int32)
         img = tf.reshape(img, tf.stack([FLAGS.origin_height, FLAGS.origin_width, FLAGS.origin_channel]))
-        img = image_preprocessing_fn(img, FLAGS.origin_height, FLAGS.origin_widths)
+        img = image_preprocessing_fn(img, FLAGS.origin_height, FLAGS.origin_width)
         # img = tf.image.resize_images(img, [FLAGS.origin_height, FLAGS.origin_width], 0)
 
         label = features['label']
