b968fa541bd765703b4be8a6e90bed47adbdb46b
zhihuikaicheng <314696702@qq.com>
2018-01-12 16:28:37 +0800

    zkc change dir

--- scripts/get_gallery_features_single.sh
+++ scripts/get_gallery_features_single.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Where the pre-trained InceptionV3 checkpoint is saved to.
-PRETRAINED_CHECKPOINT_DIR=/home/yuanziyi/checkpoints
+PRETRAINED_CHECKPOINT_DIR=/world/date-gpu-94/sysu-reid/checkpoints
 # Where the training (fine-tuned) checkpoint and logs will be saved to.
 TRAIN_DIR=/world/data-gpu-94/sysu-reid/checkpoints/inception_v3_299
 # Where the dataset is saved to.
