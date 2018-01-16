64d0a838cedfd67843c6a2200fee386f6e2eaf2d
Frankziyi <617328440@qq.com>
2018-01-16 16:08:07 +0800

    test 299

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,24 +1,24 @@
 
 python eval.py \
---label_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/17902/test_gallery_labels.mat \
---feature_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/17902/test_gallery_features.mat \
---label_probe_path=/world/data-gpu-94/sysu-reid/multiscale/17902/test_probe_labels.mat \
---feature_probe_path=/world/data-gpu-94/sysu-reid/multiscale/17902/test_probe_features.mat \
---cam_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/17902/testCAM.mat \
---cam_probe_path=/world/data-gpu-94/sysu-reid/multiscale/17902/queryCAM.mat 
+--label_gallery_path=/home/yuanziyi/multiscale_master/multiscale/17902/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/multiscale_master/multiscale/17902/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/multiscale_master/multiscale/17902/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/multiscale_master/multiscale/17902/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/multiscale_master/multiscale/17902/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/multiscale_master/multiscale/17902/queryCAM.mat 
 
 python eval.py \
---label_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/35607/test_gallery_labels.mat \
---feature_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/35607/test_gallery_features.mat \
---label_probe_path=/world/data-gpu-94/sysu-reid/multiscale/35607/test_probe_labels.mat \
---feature_probe_path=/world/data-gpu-94/sysu-reid/multiscale/35607/test_probe_features.mat \
---cam_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/35607/testCAM.mat \
---cam_probe_path=/world/data-gpu-94/sysu-reid/multiscale/35607/queryCAM.mat 
+--label_gallery_path=/home/yuanziyi/multiscale_master/multiscale/35607/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/multiscale_master/multiscale/35607/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/multiscale_master/multiscale/35607/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/multiscale_master/multiscale/35607/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/multiscale_master/multiscale/35607/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/multiscale_master/multiscale/35607/queryCAM.mat 
 
 python eval.py \
---label_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/42022/test_gallery_labels.mat \
---feature_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/42022/test_gallery_features.mat \
---label_probe_path=/world/data-gpu-94/sysu-reid/multiscale/42022/test_probe_labels.mat \
---feature_probe_path=/world/data-gpu-94/sysu-reid/multiscale/42022/test_probe_features.mat \
---cam_gallery_path=/world/data-gpu-94/sysu-reid/multiscale/42022/testCAM.mat \
---cam_probe_path=/world/data-gpu-94/sysu-reid/multiscale/42022/queryCAM.mat 
+--label_gallery_path=/home/yuanziyi/multiscale_master/multiscale/42022/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/multiscale_master/multiscale/42022/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/multiscale_master/multiscale/42022/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/multiscale_master/multiscale/42022/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/multiscale_master/multiscale/42022/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/multiscale_master/multiscale/42022/queryCAM.mat 
