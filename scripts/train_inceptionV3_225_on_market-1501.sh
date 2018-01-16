19fce761e97d08d3e3b6a6bbb8296a5740aa8dd7
智慧 <zhihuikaicheng@zhihuideMacBook-Pro.local>
2018-01-16 16:34:41 +0800

    change sh

--- scripts/train_inceptionV3_225_on_market-1501.sh
+++ scripts/train_inceptionV3_225_on_market-1501.sh
@@ -2,7 +2,7 @@
 # Where the pre-trained InceptionV3 checkpoint is saved to.
 PRETRAINED_CHECKPOINT_DIR=/world/data-gpu-94/sysu-reid/checkpoints
 # Where the training (fine-tuned) checkpoint and logs will be saved to.
-TRAIN_DIR=/world/data-gpu-94/sysu-reid/checkpoints/inception_v3_299
+TRAIN_DIR=/world/data-gpu-94/sysu-reid/checkpoints/inception_v3_225
 # Where the dataset is saved to.
 DATASET_DIR=/world/data-gpu-94/sysu-reid/zhangkaicheng/Market-1501
 # WHere the log is saved to
