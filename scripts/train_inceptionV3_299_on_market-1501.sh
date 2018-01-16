ee373786b1ad69622741f5df46fa8c5a2a81bef7
Frankziyi <617328440@qq.com>
2018-01-16 11:31:11 +0800

    debug

--- scripts/train_inceptionV3_299_on_market-1501.sh
+++ scripts/train_inceptionV3_299_on_market-1501.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Where the pre-trained InceptionV3 checkpoint is saved to.
-PRETRAINED_CHECKPOINT_DIR=/world/data-gpu-94/sysu-reid/checkpoints
+PRETRAINED_CHECKPOINT_DIR=/world/data-gpu-94/sysu-reid/checkpoints/ziyi
 # Where the training (fine-tuned) checkpoint and logs will be saved to.
-TRAIN_DIR=/world/data-gpu-94/sysu-reid/checkpoints/inception_v3_299
+TRAIN_DIR=/world/data-gpu-94/sysu-reid/checkpoints/test/inception_v3_299
 # Where the dataset is saved to.
 DATASET_DIR=/world/data-gpu-94/sysu-reid/zhangkaicheng/Market-1501
 # WHere the log is saved to
